ed139d62b56fc6fedcf1e3ec72da25fdc56b3551
shota <116172579+lxizaxi@users.noreply.github.com>
2025-05-25 17:11:22 +0900

    hoge

--- src/components/SkillBadge.tsx
+++ src/components/SkillBadge.tsx
@@ -9,7 +9,7 @@ interface SkillBadgeProps {
 const SkillBadge: React.FC<SkillBadgeProps> = ({ name, iconSrc, altText="" }) => {
   return (
     <div className="badge badge-xs">
-      <Image src={iconSrc} alt={altText} width={16} height={16} />&nbsp;{name}
+      <Image src={iconSrc} alt={altText} width={16} height={16} unoptimized />&nbsp;{name}
     </div>
   );
 };
