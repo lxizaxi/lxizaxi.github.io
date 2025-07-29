292f55324bf42cf9a2cb6aec889131d0c2e7d6e6
shota <116172579+lxizaxi@users.noreply.github.com>
2025-07-29 12:34:30 +0900

    hoge

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -47,7 +47,7 @@ export default function HeroSection() {
               className="text-xl text-muted-foreground mb-6"
               variants={childVariants}
             >
-              System Engineer 👨‍💻
+              @lxizaxi
             </motion.p>
 
             <motion.div
